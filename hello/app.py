b2819cbce44afbbceb6a5ead3564e074bb63ac2c
Deepak Puthraya <deepakputhraya@gmail.com>
2019-04-01 19:55:23 +0530

    Add versions info to file

--- hello/app.py
+++ hello/app.py
@@ -8,5 +8,10 @@ def hello():
     return {"message": "hello"}
 
 
+@app.route("/version")
+def world():
+    return {"version": "v1.0"}
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
--- world/app.py
+++ world/app.py
@@ -8,5 +8,10 @@ def world():
     return {"message": "world"}
 
 
+@app.route("/version")
+def world():
+    return {"version": "v1.0"}
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=4000)
